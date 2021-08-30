8fe7ac4f723e5efc5a0491a5ee2b20bc8f9b5f2b
FREDYKA MAULANA <fredyka.maulana@gmail.com>
2021-08-30 10:24:14 +0700

    login commit

--- app/src/main/java/com/miniapp/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/miniapp/app/ui/login/LoginActivity.kt
@@ -1,9 +1,19 @@
 package com.miniapp.app.ui.login
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.miniapp.app.R
 import com.miniapp.app.databinding.ActivityLoginBinding
 import com.miniapp.app.ui.MainActivity
@@ -16,27 +26,41 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : BaseActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var callbackManager: CallbackManager
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     private val vm: LoginViewModel by viewModel()
+
+    companion object{
+        const val PUBLIC_PROFILE = "public_profile"
+        const val EMAIL = "email"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        callbackManager = CallbackManager.Factory.create()
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK) loginEvent()
+        }
         setupToolbar(binding.toolbar, "")
 
         setupObserver()
         setupView()
     }
 
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
     private fun setupObserver() {
         vm.loginStatus.observe(this, {
-            if(it) {
-                binding.groupLoading.visibility = View.GONE
+            if(it) { binding.groupLoading.visibility = View.GONE
                 startActivity(Intent(this@LoginActivity, MainActivity::class.java))
                 finish()
-            } else {
-                binding.groupLoading.visibility = View.VISIBLE
-            }
+            } else { binding.groupLoading.visibility = View.VISIBLE }
         })
     }
 
@@ -44,17 +68,12 @@ class LoginActivity : BaseActivity() {
         binding.run {
             btnLogin.setOnClickListener {
                 val filled = binding.etUsername.text?.isNotEmpty() ?: false &&
-                        binding.etPassword.text?.isNotEmpty() ?: false
+                             binding.etPassword.text?.isNotEmpty() ?: false
                 if(filled) loginEvent()
                 else showSnackBar(binding.root, "Harap isi username dan password", "OK")
             }
-            btnFbLogin.setOnClickListener {
-                //LoginManager.getInstance().logInWithReadPermissions(this@LoginActivity, listOf("public_profile", "email"))
-                showSnackBar(binding.root, "Fitur dalam pengembangan", "OK")
-            }
-            btnGoogleLogin.setOnClickListener {
-                showSnackBar(binding.root, "Fitur dalam pengembangan", "OK")
-            }
+            btnFbLogin.setOnClickListener { fbSignIn() }
+            btnGoogleLogin.setOnClickListener { googleSignIn() }
             btnLoginWithFingerprint.setOnClickListener {
                 showSnackBar(binding.root, "Fitur dalam pengembangan", "OK")
             }
@@ -75,5 +94,28 @@ class LoginActivity : BaseActivity() {
                 vm.loginStatus(true)
             }
         }
+    }
+
+    private fun googleSignIn(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val client = GoogleSignIn.getClient(this, gso)
+        //val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        val signInIntent = client.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private fun fbSignIn(){
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf(PUBLIC_PROFILE, EMAIL))
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(result: LoginResult?) { loginEvent() }
+
+                override fun onCancel() { showSnackBar(binding.root,"Login Cancelled") }
+
+                override fun onError(error: FacebookException?) { showSnackBar(binding.root, error?.message ?: "") }
+            })
     }
 }
